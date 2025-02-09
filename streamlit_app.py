73068d50d5aa21308916dd2787d07087bd39969f
Sarabjeet Singh <sarabjeet.singh@prxsion.com>
2025-02-09 19:59:50 +0530

    image path changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Rightank",
-    page_icon="assets/favicon.png",
+    page_icon="assets\\favicon.png",
     layout="wide",
 )
 
 st.write('Rightank - Model Marketplace')
-st.image("assets/logo.png")
+st.image("assets\\logo.png")
