974c49c49432c729c206608a55da244fefa9cba4
Sarabjeet Singh <sarabjeet.singh@prxsion.com>
2025-02-09 20:49:49 +0530

    images

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from PIL import Image
+
+logo_png = Image.open("static\\logo.png")
+favicon_png = Image.open("static\\favicon.png")
 
 st.set_page_config(
-    page_title="Rightank",
-    page_icon="static\\favicon.png",
-    layout="wide",
+    page_title = "Rightank",
+    page_icon = favicon_png,
+    layout = "wide",
 )
 
 st.write('Rightank - Model Marketplace')
-st.image("static\\logo.png")
+st.image(logo_png)
